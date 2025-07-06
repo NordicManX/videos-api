744681c04c962fd883de42fdcf741903b89c4ed8
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-07-06 19:43:45 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/nordicmanx/videos-api
+
+go 1.24.3
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+)
---
+++ internal/repository/category_repository.go
@@ -0,0 +1,76 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/nordicmanx/videos-api"
+	"github.com/nordicmanx/videos-api/internal/models"
+)
+
+type CategoryRepository struct {
+
+	DB *pgxpool.Pool
+}
+func NewCategoryRepository(db *pgxpool.Pool) *CategoryRepository {
+	return &CategoryRepository{DB: db}
+}
+
+func (r *CategoryRepository) CreateCategory(ctx context.Context, name string) (*models.Category, error) {
+	category := &models.Category{
+		Name: name,
+	}
+
+	query := `INSERT INTO categories (name) VALUES ($1) RETURNING id, created_at, updated-at`
+	err := r.DB.QueryRow(ctx, query, name).Scan(&category.ID, &category.CreatedAt, &category.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (r *CategoryRepository) GetAllCategories(ctx context.Context) ([]models.Category, error) {
+	var categories []models.Category
+	query := `SELECT id, name, created_at, updated_at FROM categories ORDERBY name ASC`
+
+	rows, err := r.DB.Query(ctx, query)
+	if err != nil {	
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var category := models.Category
+		if err := rows.Scan(&category.ID, &category.Name, &category.CreatedAt, &category.UpdatedAt); 
+		err != nil {
+			return nil, err
+			}
+		categories = append(categories, category)
+		}
+		return categories, nil
+
+}
+
+func (r *CategoryRepository) GetCategoryByID(ctx context.Context, id uuid.UUID) (*models.Category, error) {
+	var category models.Category
+	query := `SELECT id, name,created_at, updated_at FROM categories WHERE id = $1`
+	err := r.DB.QueryRow(ctx, query, id).Scan(&category.ID, &category.Name, &category.CreatedAt, &category.UpdatedAt) 
+	if err != nil {
+		return nil, err 
+	}
+	return &category, nil
+}
+
+func (r *CategoryRepository) UpdateCategory(ctx context.Context, id uuid.UUID, name string) error {
+	query := `UPDATE categories SET name = $1, updated_at = $2 WHERE id = $3`
+	_, err := r.DB.Exec(ctx, query, name, time.Now(), id)
+	return err
+}
+
+func (r CategoryRepository) DeleteCategory(ctx context.Context, id uuid.UUID) error {
+	query := `DELETE FROM categories WHERE id =$1`
+	_, err := r.DB.Exec(ctx, query, id)
+	return err
+}
